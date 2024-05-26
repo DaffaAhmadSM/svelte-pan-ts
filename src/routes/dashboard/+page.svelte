58eea9b1c8322b390ec728515983fe9c3adec3b1
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2024-05-26 09:51:14 +0700

    menu sidebar

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,5 @@
-<div class="flex w-full float-right flex-col ">
-    <h1>Dashboard</h1>
-    <p>This is a protected route</p>
+<div class="flex w-full flex-col justify-center items-center">
+    <button>Dashboard</button>
+    <p>This is a dashboard route</p>
     <p>Only authenticated users can access this page</p>
 </div>
--- src/routes/dashboard/nested.svelte
+++ src/routes/dashboard/nested.svelte
@@ -1,8 +1,10 @@
 <script>
+	import { fade } from "svelte/transition";
+
     export let menu;
     export let indent = 0;
 	
-	let open = true;
+	let open = false;
 
     function toggle() {
         open = !open;
@@ -12,12 +14,16 @@
 <li style="transform: translate({indent}px);" class="flex w-full hover:bg-gray-100 dark:hover:bg-gray-700 group text-start">
     <a class="pt-1 pr-1 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:underline" href={menu.url}>{menu.name}</a>
     {#if menu.children}
-        <button on:click={toggle}>b</button>
+        {#if open}
+            <button on:click={toggle}>></button>
+        {:else}
+            <button on:click={toggle}>v</button>
+        {/if}
     {/if}
 </li>
 
 {#if open && menu.children}
         {#each menu.children as item}
-            <svelte:self menu={item} indent={indent + 24} />
+            <svelte:self menu={item} indent={indent + 24}/>
         {/each}
 {/if}
