c42b1bb99e30588dcb259830337fe94a5cecfeca
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2024-07-03 09:40:36 +0700

    add customer holiday menu

--- src/routes/admin/customer/customer-calendar-holiday/+page.svelte
+++ src/routes/admin/customer/customer-calendar-holiday/+page.svelte
@@ -43,37 +43,13 @@
         });
         customerAll = await res.json();
     }
-    async function getNumberSequenceAll(){
-        const res = await fetch(import.meta.env.VITE_API_URL + '/number-sequence/all', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': 'Bearer ' + getCookie('token')
-            }
-        });
-        numberSequenceAll = await res.json();
-    }
-
-    async function numberSequenceChange(e){
-        const toastId = toastTriggerLoading("Loading...");
-        const res = await fetch(import.meta.env.VITE_API_URL + '/number-sequence/detail/' + e.target.value, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': 'Bearer ' + getCookie('token')
-            }
-        });
-        const data = await res.json();
-        toastTriggerUpdate("Data Updated", toastId);
-        formData.code = data.data.code +  data.data.current_number;
-    }
 
 
-  const fetchUrl = '/customer-contract/list';
-  const updateUrl = '/customer-contract/update';
-  const deleteUrl = '/customer-contract/delete';
-  const createUrl = '/customer-contract/create';
-  const detailUrl = '/customer-contract/detail';
+  const fetchUrl = '/customer-calendar-holiday/list';
+  const updateUrl = '/customer-calendar-holiday/update';
+  const deleteUrl = '/customer-calendar-holiday/delete';
+  const createUrl = '/customer-calendar-holiday/create';
+  const detailUrl = '/customer-calendar-holiday/detail';
 </script>
 
 <div class="w-full overflow-auto">
@@ -83,23 +59,12 @@
     <UniversalSetupTable data={data} fetchUrl={fetchUrl} deleteUrl={deleteUrl} updateUrl={updateUrl} detailUrl={detailUrl} createUrl={createUrl} bind:formData={formData} tableList={tableList}>
         <svelte:fragment slot="table-row" let:row let:index>
             <td class="table-td">{index + 1}</td>
-            <td class="table-td">{row.code}</td>
-            <td class="table-td">{row.contract_no}</td>
-            <td class="table-td">{row.description}</td>
+            <td class="table-td">{row.date}</td>
+            <td class="table-td">{row.remarks}</td>
             <td class="table-td">{row.customer.no}</td>
             <td class="table-td">{row.customer.name}</td>
         </svelte:fragment>
         <svelte:fragment slot="aditional-form-create">
-            <fieldset class="table-fieldset">
-                <div class="table-field-label">Number Sequence</div>
-                <select name="number_sequence" bind:value={formData.number_sequence_id} class="table-field-input" on:change={numberSequenceChange}>
-                    {#if numberSequenceAll}
-                        {#each numberSequenceAll.data as numberSequence}
-                            <option value={numberSequence.id}>{numberSequence.code}</option>
-                        {/each}
-                    {/if}
-                </select>
-            </fieldset>
             <fieldset class="table-fieldset">
                 <div class="table-field-label">Customer</div>
                 <select name="number_sequence" bind:value={formData.customer_id} class="table-field-input">
@@ -113,16 +78,6 @@
         </svelte:fragment>
         <svelte:fragment slot="aditional-form-update">
             <fieldset class="table-fieldset">
-                <div class="table-field-label">Number Sequence</div>
-                <select name="number_sequence" bind:value={formData.number_sequence_id} class="table-field-input" on:change={numberSequenceChange}>
-                    {#if numberSequenceAll}
-                        {#each numberSequenceAll.data as numberSequence}
-                            <option value={numberSequence.id}>{numberSequence.code}</option>
-                        {/each}
-                    {/if}
-                </select>
-            </fieldset>
-            <fieldset class="table-fieldset">
                 <div class="table-field-label">Customer</div>
                 <select name="number_sequence" bind:value={formData.customer_id} class="table-field-input">
                     {#if customerAll}
@@ -134,10 +89,10 @@
             </fieldset>
         </svelte:fragment>
         <svelte:fragment slot="add-row" let:nullform={nullform} let:openAddRow>
-            <button class="button-table-add" on:click={() =>  {openAddRow(); nullform(); getNumberSequenceAll(); getCustomerAll();}}><p>Add</p></button>
+            <button class="button-table-add" on:click={() =>  {openAddRow(); nullform(); getCustomerAll();}}><p>Add</p></button>
         </svelte:fragment>
         <svelte:fragment slot="edit-row" let:prop={row} let:detailTable={detailTable}>
-            <button class="btn btn-warning hover:btn-error" on:click={() =>  {detailTable(row.id); getNumberSequenceAll(); getCustomerAll();}}>Edit</button>
+            <button class="btn btn-warning hover:btn-error" on:click={() =>  {detailTable(row.id); getCustomerAll();}}>Edit</button>
         </svelte:fragment>
     </UniversalSetupTable>
 </div>
