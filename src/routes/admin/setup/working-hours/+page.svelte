efacc264d1e0df27dae5381139f8c5474b9371cd
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2024-07-31 10:59:34 +0700

    working hour: detail

--- src/routes/admin/setup/working-hours/+page.svelte
+++ src/routes/admin/setup/working-hours/+page.svelte
@@ -19,6 +19,37 @@
             type: "text"
         }
     ]
+    
+    let detailMeta = [
+        {
+            name: "Code",
+            id: "code",
+            type: "text"
+        },
+        {
+            name: "Description",
+            id: "description",
+            type: "text"
+        },
+        {
+            name: "Working Hour Detail",
+            id: "working_hour_detail",
+            type: "array",
+            arrayGuide: [
+                {
+                    name: "Day",
+                    main_key: true,
+                    id: "day",
+                    type: "text"
+                },
+                {
+                    name: "Hours",
+                    id: "hours",
+                    type: "text"
+                },
+            ]
+        }
+    ]
 
 
   const fetchUrl = '/working-hour/list';
@@ -30,5 +61,5 @@
 </script>
 
 <div class="table-container">
-    <UniversalSetupTable data={data} fetchUrl={fetchUrl} deleteUrl={deleteUrl} updateUrl={updateUrl} detailUrl={detailUrl} createUrl={createUrl} formData={formData} tableList={tableList} {namePage}></UniversalSetupTable>
+    <UniversalSetupTable {detailMeta} data={data} fetchUrl={fetchUrl} deleteUrl={deleteUrl} updateUrl={updateUrl} detailUrl={detailUrl} createUrl={createUrl} formData={formData} tableList={tableList} {namePage}></UniversalSetupTable>
 </div>
