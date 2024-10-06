721d0e6dc0c780f1266db546662864af2c4aa95c
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2024-10-06 21:43:29 +0700

    add timesheet data menu

---
+++ src/routes/admin/timesheet/timesheet-data/+page.server.js
@@ -0,0 +1,46 @@
+import { error } from '@sveltejs/kit'
+
+export  async function load({ fetch, cookies, url }) {
+    const list = await fetch(import.meta.env.VITE_API_URL + '/timesheet-data/list', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + cookies.get('token')
+        }
+    })
+
+    const paramsurl = url.searchParams.get('menuid')
+    const permission = await fetch(import.meta.env.VITE_API_URL + '/menu-user-permission/menu/' + paramsurl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + cookies.get('token')
+        }
+    })
+
+    if (!list.ok) {
+        if (import.meta.env.VITE_DEBUG == "true") {
+            return error(500, { 
+                message: await list.text(),
+                debug: true
+            })
+        }
+        return error(500, { 
+            message: 'Error',
+        })
+    }
+
+    if (!permission.ok) {
+        if (import.meta.env.VITE_DEBUG == "true") {
+            return error(500, { 
+                message: await permission.text(),
+                debug: true
+            })
+        }
+        return error(500, { 
+            message: 'Error',
+        })
+    }
+
+    return { list: await list.json(), permissions: await permission.json(), paramsurl: paramsurl }
+}
---
+++ src/routes/admin/timesheet/timesheet-data/[slug]/+page.server.js
@@ -0,0 +1,32 @@
+import { error } from '@sveltejs/kit'
+
+export  async function load({ fetch, cookies, url, params }) {
+
+    const paramsurl = url.searchParams.get('menuid')
+    const list = await fetch(import.meta.env.VITE_API_URL + '/timesheet-data/detail/' + params.slug, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + cookies.get('token')
+        }
+    })
+    if (!list.ok) {
+        if (import.meta.env.VITE_DEBUG == "true") {
+            return error(500, { 
+                message: await list.text(),
+                debug: true
+            })
+        }
+        return error(500, { 
+            message: 'Error',
+        })
+    }
+
+    return { list: await list.json(), permissions: {
+        permission : {
+            create: false,
+            update: false,
+            delete: false
+        },
+    }, slug: params.slug, paramsurl: paramsurl }
+}
