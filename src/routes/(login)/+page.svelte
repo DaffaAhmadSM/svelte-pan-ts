e86bfc6d55c353e302cabec9aa15856ff5d37149
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2024-10-08 10:30:35 +0700

    change login behavior 1

--- src/routes/(login)/+page.svelte
+++ src/routes/(login)/+page.svelte
@@ -17,7 +17,7 @@
             } else {
                 document.cookie = `token=${data.token}`;
                 localStorage.setItem('menu', JSON.stringify(data.menu));
-                window.location.href = '/dashboard';
+                window.location.href = '/admin';
             }
         })
         .catch(error => {
@@ -28,7 +28,7 @@
     
     <div class="h-screen w-screen flex items-center justify-center">
         <div class="w-screen">
-            <form class="max-w-sm mx-auto" method="POST">
+            <div class="max-w-sm mx-auto">
                 <div class="relative -mb-px h-px w-full bg-gradient-to-r from-transparent via-sky-300 to-transparent"></div>
                 <div class="mb-5">
                 <h1 class="text-4xl">Login</h1>
@@ -39,7 +39,7 @@
                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Your password</label>
                 <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required bind:value={password}/>
                 </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
-            </form>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center" on:click={login}>Submit</button>
+            </div>
         </div>
     </div>
