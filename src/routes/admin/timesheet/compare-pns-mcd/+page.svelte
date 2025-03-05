4faa954232fe5a43403b66cc5cdeb78fc6a9c586
DaffaAhmadSM <Daffamubaroqi321@gmail.com>
2025-03-05 12:34:35 +0700

    move overtime calculation to timesheet

--- src/routes/admin/timesheet/compare-pns-mcd/+page.svelte
+++ src/routes/admin/timesheet/compare-pns-mcd/+page.svelte
@@ -8,6 +8,7 @@
 	import { getCookie } from '$lib/helpers/getLocalCookies';
 	import { holdInput } from '$lib/stores/holdinput.js';
 	import { onMount } from 'svelte';
+	import { Rows } from 'lucide-svelte';
 	export let data;
 
 	// long pooling every 10 sec
@@ -245,7 +246,31 @@
 		dataDetail = true;
 		formData = dumpformData;
 		holdInput.set(false);
-		return toastTrigger('importing data', toastId, 200);
+		return toastTrigger('data imported', toastId, 200);
+	}
+
+	async function overtimeCalculation(tempsheetID, isrecalculate = false) {
+		const toastId = toastTriggerLoading('Calculating...');
+		const res = await fetch(
+			import.meta.env.VITE_API_URL + '/temp-timesheet/calculate-overtime/queue/' + tempsheetID,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: 'Bearer ' + getCookie('token')
+				},
+
+				body: JSON.stringify({
+					recalculate: isrecalculate
+				})
+			}
+		);
+		if (res.ok) {
+			data.list = await fetchTable();
+			return toastTrigger('Data Calculated', toastId, 200, 500);
+		}
+
+		return toastTrigger(data.list.data.message, toastId, res.status);
 	}
 
 	async function deleteRow(id) {
@@ -421,9 +446,16 @@
                     Detail
                 </button> -->
 										{#if (row.pns_mcd_diff_count == 0 && row.status == 'draft') || row.status == 'calculated'}
-											<a
+											<button
 												class="flex gap-2"
-												href="/admin/timesheet/compare-pns-mcd/overtimecalc/{row.random_string}?menuid={data.paramsurl}"
+												on:click={() => {
+													if (row.status == 'calculated') {
+														overtimeCalculation(row.random_string, true);
+														return;
+													} else {
+														overtimeCalculation(row.random_string, false);
+													}
+												}}
 											>
 												<svg
 													width="18px"
@@ -446,11 +478,55 @@
 														></path>
 													</g></svg
 												>
-												<p>Overtime Calculation</p>
-											</a>
+
+												{#if row.status == 'calculated'}
+													<span>Recalculate Overtime</span>
+												{:else}
+													<span>Calculate Overtime</span>
+												{/if}
+											</button>
 										{/if}
 
-										{#if row.status != 'moved' && row.status != 'draft' && row.status != 'cancelled'}
+										{#if row.status == 'calculated'}
+											<button class="flex gap-2" on:click={() => moveToTimesheet(row.id)}>
+												<p>Move to Timesheet</p>
+											</button>
+										{/if}
+
+										{#if row.status == 'draft'}
+											<button
+												class="btn btn-primary hover:btn-error flex gap-2"
+												on:click={() => deleteRow(row.id)}
+											>
+												<svg
+													width="18px"
+													height="18px"
+													viewBox="0 0 24 24"
+													fill="none"
+													xmlns="http://www.w3.org/2000/svg"
+												>
+													<g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+													<g
+														id="SVGRepo_tracerCarrier"
+														stroke-linecap="round"
+														stroke-linejoin="round"
+													/>
+
+													<g id="SVGRepo_iconCarrier">
+														<path
+															d="M8 1.5V2.5H3C2.44772 2.5 2 2.94772 2 3.5V4.5C2 5.05228 2.44772 5.5 3 5.5H21C21.5523 5.5 22 5.05228 22 4.5V3.5C22 2.94772 21.5523 2.5 21 2.5H16V1.5C16 0.947715 15.5523 0.5 15 0.5H9C8.44772 0.5 8 0.947715 8 1.5Z"
+															fill="#e01b24"
+														/>
+														<path
+															d="M3.9231 7.5H20.0767L19.1344 20.2216C19.0183 21.7882 17.7135 23 16.1426 23H7.85724C6.28636 23 4.98148 21.7882 4.86544 20.2216L3.9231 7.5Z"
+															fill="#e01b24"
+														/>
+													</g>
+												</svg>
+												<p>delete</p>
+											</button>
+											<!-- {:else}
 											<button
 												class="flex gap-1"
 												on:click={() => {
@@ -493,53 +569,11 @@
 													</g></svg
 												>
 												<p>Cancel</p>
-											</button>
+											</button> -->
 										{/if}
-
-										{#if row.status == 'verified'}
-											<button class="flex gap-2" on:click={() => moveToTimesheet(row.id)}>
-												<p>Move to Timesheet</p>
-											</button>
+										{#if row.status == 'importing ...' || row.status == 'calculating' || row.status == 'recalculating'}
+											<div class="loading"></div>
 										{/if}
-
-										{#if row.status == 'draft' || row.status == 'cancelled'}
-											<button
-												class="btn btn-primary hover:btn-error flex gap-2"
-												on:click={() => deleteRow(row.id)}
-											>
-												<svg
-													width="18px"
-													height="18px"
-													viewBox="0 0 24 24"
-													fill="none"
-													xmlns="http://www.w3.org/2000/svg"
-												>
-													<g id="SVGRepo_bgCarrier" stroke-width="0" />
-
-													<g
-														id="SVGRepo_tracerCarrier"
-														stroke-linecap="round"
-														stroke-linejoin="round"
-													/>
-
-													<g id="SVGRepo_iconCarrier">
-														<path
-															d="M8 1.5V2.5H3C2.44772 2.5 2 2.94772 2 3.5V4.5C2 5.05228 2.44772 5.5 3 5.5H21C21.5523 5.5 22 5.05228 22 4.5V3.5C22 2.94772 21.5523 2.5 21 2.5H16V1.5C16 0.947715 15.5523 0.5 15 0.5H9C8.44772 0.5 8 0.947715 8 1.5Z"
-															fill="#e01b24"
-														/>
-														<path
-															d="M3.9231 7.5H20.0767L19.1344 20.2216C19.0183 21.7882 17.7135 23 16.1426 23H7.85724C6.28636 23 4.98148 21.7882 4.86544 20.2216L3.9231 7.5Z"
-															fill="#e01b24"
-														/>
-													</g>
-												</svg>
-												<p>delete</p>
-											</button>
-										{/if}
-									</td>
-								{:else if row.status == 'importing ...'}
-									<td class="table-td flex items-center justify-center gap-4">
-										<div class="loading"></div>
 									</td>
 								{/if}
 							</tr>
